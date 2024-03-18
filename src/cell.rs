b4f21f84067becc42dab140af220bff9157a5307
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-18 20:35:51 -0300

    refactoring cell to follow the original article

--- src/cell.rs
+++ src/cell.rs
@@ -1,7 +1,13 @@
 #[derive(Debug)]
+pub enum Label {
+    PRINCIPAL,
+    AUXILIAR,
+}
+
+#[derive(Debug)]
 // where connected_to is the cell index on a net and the port index on a cell port
 pub struct Port {
-    pub connected_to: Option<(usize, usize)>,
+    pub label: Label,
 }
 
 // a cell has arity >= 0, that has one principal port and n auxiliary_ports
@@ -10,12 +16,13 @@ pub struct Cell {
     pub arity: usize,
     pub principal_port: Port,
     pub auxiliary_ports: Vec<Port>,
+    pub label: Label,
 }
 
 #[derive(Debug)]
 pub struct Wire {
-    from: Option<(usize, usize)>,
-    to: Option<(usize, usize)>,
+    pub from: Option<(usize, usize)>,
+    pub to: Option<(usize, usize)>,
 }
 
 // a net is a graph consisting of a finite number of cells and an extra set of free ports, each
@@ -23,15 +30,29 @@ pub struct Wire {
 #[derive(Debug)]
 pub struct InteractionNet {
     cells: Vec<Cell>,
+    wires: Vec<Wire>,
+    free_ports: Vec<Port>,
 }
 
 impl InteractionNet {
     pub fn new() -> InteractionNet {
-        InteractionNet { cells: Vec::new() }
+        InteractionNet {
+            cells: Vec::new(),
+            wires: Vec::new(),
+            free_ports: Vec::new(), // Initializing free ports collection
+        }
     }
 
     pub fn add_cell(&mut self, cell: Cell) {
         self.cells.push(cell);
+    }
+
+    pub fn add_free_port(&mut self, port: Port) {
+        self.free_ports.push(port);
+    }
+
+    pub fn add_wire(&mut self, wire: Wire) {
+        self.wires.push(wire);
     }
 
     pub fn connect_ports(
@@ -41,11 +62,10 @@ impl InteractionNet {
         to_cell: usize,
         to_port: usize,
     ) {
-        if let Some(from_port) = self.cells[from_cell].auxiliary_ports.get_mut(from_port) {
-            from_port.connected_to = Some((to_cell, to_port));
-        }
-        if let Some(to_port) = self.cells[to_cell].auxiliary_ports.get_mut(to_port) {
-            to_port.connected_to = Some((from_cell, from_port));
-        }
+        let new_wire = Wire {
+            from: Some((from_cell, from_port)),
+            to: Some((to_cell, to_port)),
+        };
+        self.add_wire(new_wire);
     }
 }
