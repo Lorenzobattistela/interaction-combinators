9c31bc77def3181a53fef846c08232fb3c07c828
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-18 19:19:37 -0300

    advancing with the fundamental structures

--- src/cell.rs
+++ src/cell.rs
@@ -1,11 +1,51 @@
-struct Port {}
+#[derive(Debug)]
+// where connected_to is the cell index on a net and the port index on a cell port
+pub struct Port {
+    pub connected_to: Option<(usize, usize)>,
+}
 
 // a cell has arity >= 0, that has one principal port and n auxiliary_ports
+#[derive(Debug)]
 pub struct Cell {
-    arity: usize,
-    principal_port: Port,
-    auxiliary_ports: Vec<Port>,
+    pub arity: usize,
+    pub principal_port: Port,
+    pub auxiliary_ports: Vec<Port>,
+}
+
+#[derive(Debug)]
+pub struct Wire {
+    from: Option<(usize, usize)>,
+    to: Option<(usize, usize)>,
 }
 
 // a net is a graph consisting of a finite number of cells and an extra set of free ports, each
 // port being connected to another one by means of a wire.
+#[derive(Debug)]
+pub struct InteractionNet {
+    cells: Vec<Cell>,
+}
+
+impl InteractionNet {
+    pub fn new() -> InteractionNet {
+        InteractionNet { cells: Vec::new() }
+    }
+
+    pub fn add_cell(&mut self, cell: Cell) {
+        self.cells.push(cell);
+    }
+
+    pub fn connect_ports(
+        &mut self,
+        from_cell: usize,
+        from_port: usize,
+        to_cell: usize,
+        to_port: usize,
+    ) {
+        if let Some(from_port) = self.cells[from_cell].auxiliary_ports.get_mut(from_port) {
+            from_port.connected_to = Some((to_cell, to_port));
+        }
+        if let Some(to_port) = self.cells[to_cell].auxiliary_ports.get_mut(to_port) {
+            to_port.connected_to = Some((from_cell, from_port));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-use interaction_combinators::net;
+use interaction_combinators::cell::{Cell, InteractionNet, Port};
 
 fn main() {
-    println!("Hello, world!");
+    // Create a new interaction net
+    let mut net = InteractionNet::new();
+
+    // Create a couple of cells
+    let cell1 = Cell {
+        arity: 2,
+        principal_port: Port { connected_to: None },
+        auxiliary_ports: vec![Port { connected_to: None }, Port { connected_to: None }],
+    };
+
+    let cell2 = Cell {
+        arity: 1,
+        principal_port: Port { connected_to: None },
+        auxiliary_ports: vec![Port { connected_to: None }],
+    };
+
+    // Add the cells to the net
+    net.add_cell(cell1);
+    net.add_cell(cell2);
+
+    // Connect ports between cells
+    net.connect_ports(0, 0, 1, 0);
+    net.connect_ports(0, 1, 1, 0);
+
+    // Print the net to see the connections
+    println!("{:#?}", net);
 }
