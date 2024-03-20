55dbce948d5c49eb183292cb6152feeba066bf16
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-20 10:44:27 -0300

    auxiliar functions to help reducing process

--- src/cell.rs
+++ src/cell.rs
@@ -2,6 +2,9 @@
 pub enum Label {
     PRINCIPAL,
     AUXILIAR,
+    SUM,
+    SUC,
+    ZERO,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -69,15 +72,47 @@ impl InteractionNet {
         self.add_wire(new_wire);
     }
 
+    pub fn get_port(&self, cell_index: usize, port_index: usize) -> Option<&Port> {
+        println!("cell_idx: {}, port_idx: {}", cell_index, port_index);
+        if let Some(cell) = self.cells.get(cell_index) {
+            if port_index == 0 {
+                Some(&cell.principal_port)
+            } else {
+                println!("{:?}", cell.auxiliary_ports);
+                cell.auxiliary_ports.get(port_index - 1)
+            }
+        } else {
+            None // Return None if the cell index is out of bounds
+        }
+    }
+
+    pub fn get_all_connections(&self, cell_index: usize, port_index: usize) -> Vec<&Wire> {
+        self.wires
+            .iter()
+            .filter(|wire| {
+                if let Some((from_cell_idx, from_port_idx)) = wire.from {
+                    if from_cell_idx == cell_index && from_port_idx == port_index {
+                        return true;
+                    }
+                }
+                if let Some((to_cell_idx, to_port_idx)) = wire.to {
+                    if to_cell_idx == cell_index && to_port_idx == port_index {
+                        return true;
+                    }
+                }
+                false
+            })
+            .collect()
+    }
+
     // returns all the wires that have two principal ports connected
     pub fn possible_reductions(&self) -> Vec<usize> {
         let mut reductions = Vec::new();
         for (i, wire) in self.wires.iter().enumerate() {
-            if let (Some((_from_cell, from_port)), Some((_to_cell, to_port))) =
-                (wire.from, wire.to)
+            if let (Some((_from_cell, from_port)), Some((_to_cell, to_port))) = (wire.from, wire.to)
             {
                 // port 0 is principal port
-                if from_port == 0 as usize && to_port == 0 as usize{
+                if from_port == 0 as usize && to_port == 0 as usize {
                     reductions.push(i);
                 }
             }
